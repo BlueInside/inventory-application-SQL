1007447aa1d3ceeb508dda0fdc04dad964ba6522
Karol <kpulawski13@gmail.com>
2024-09-14 17:19:34 +0100

    connect to db

---
+++ db/pool.js
@@ -0,0 +1,13 @@
+const pg = require('pg')
+
+const { Pool } = pg;
+
+const config = {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+}
+
+module.exports = new Pool(config);
