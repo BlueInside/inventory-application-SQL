7731aaab533ea91906712c85e1151df08d69b645
Karol <kpulawski13@gmail.com>
2024-09-15 13:13:46 +0100

    query to get game details with comments

--- db/queries.js
+++ db/queries.js
@@ -4,3 +4,28 @@ async function getAllGames() {
     const { rows } = await pool.query('SELECT * FROM games');
     return rows;
 }
+
+async function getGame(id) {
+    const { rows: gameRows } = await pool.query(`
+       SELECT 
+       games.name AS game_name,
+       publishers.name AS publisher_name,
+       games.description,    
+       games.image,
+       games.rate   
+       FROM games
+        LEFT JOIN publishers ON games.publisher_id = publishers.id
+        WHERE games.id = $1;
+     `, [id]);
+
+    const { rows: commentRows } = await pool.query(`
+            SELECT * FROM comments
+            WHERE comments.game_id = $1
+        `, [id])
+    return { ...gameRows[0], comments: commentRows }
+}
+
+module.exports = {
+    getAllGames,
+    getGame
+}
