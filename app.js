9fe8fd824932e8da846aac77ed26128f78493168
Karol <kpulawski13@gmail.com>
2024-09-15 09:41:33 +0100

    Add game, and main controllers

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 require('dotenv').config();
 
+const mainPageController = require('./controllers/main');
+const gameController = require('./controllers/game')
 const app = express();
 const port = process.env.PORT
+
+const gameRouter = require('./routes/game');
+
+app.use(express.urlencoded());
+
+app.get('/', mainPageController.getMainPage);
+
+app.use('/game', gameRouter);
+
+
+
 
 app.listen(port, () => {
     console.log('Inventory app listening on port: ' + port)
