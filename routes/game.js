b17399266936d84bf02b733687b0efdc97c47416
Karol <kpulawski13@gmail.com>
2024-09-15 10:17:48 +0100

    adjust paths

--- routes/game.js
+++ routes/game.js
@@ -2,10 +2,12 @@ const express = require('express');
 const gameController = require('../controllers/game')
 const gameRouter = express.Router();
 
-gameRouter.get('/game/:gameId', gameController.getGameDetails);
+gameRouter.get('/:gameId', gameController.getGameDetails);
 
-gameRouter.post('/game', gameController.addGame);
+gameRouter.post('/', gameController.addGame);
 
-gameRouter.delete('/game/:gameId', gameController.deleteGame);
+gameRouter.put('/:gameId', gameController.updateGame)
+
+gameRouter.delete('/:gameId', gameController.deleteGame);
 
 module.exports = gameRouter
