3055ab401bf5e117092d084ce5672df1cf1fb9c9
Karol <kpulawski13@gmail.com>
2024-09-15 10:17:18 +0100

    Set up pug as view engine

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('node:path');
 require('dotenv').config();
+
+const app = express();
 
 const mainPageController = require('./controllers/main');
 const gameController = require('./controllers/game')
-const app = express();
 const port = process.env.PORT
 
 const gameRouter = require('./routes/game');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(express.urlencoded());
 
