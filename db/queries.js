215a830a8645a8a755a29d5924fd7caad3703ba3
Karol <kpulawski13@gmail.com>
2024-09-19 19:52:21 +0100

    implement getAllGames query

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,44 @@
 const pool = require('./pool');
 
-async function getAllGames() {
-    const { rows } = await pool.query('SELECT * FROM games');
+async function getAllGames(title, publisher, category) {
+    let query = `
+SELECT DISTINCT games.*, publishers.name AS publisher_name
+        FROM games
+        JOIN publishers ON games.publisher_id = publishers.id
+        LEFT JOIN game_categories ON games.id = game_categories.game_id
+        LEFT JOIN categories ON game_categories.category_id = categories.id
+`;
+
+    let conditions = [];
+    let values = [];
+    let index = 1;
+
+    if (title) {
+        conditions.push(`title ILIKE $${index}`)
+        values.push(`%${title}%`)
+        index++;
+    }
+
+    if (publisher) {
+        conditions.push(`publisher_id = $${index}`);
+        values.push(publisher);
+        index++
+    }
+
+    if (category) {
+        conditions.push(`category_id = $${index}`);
+        values.push(category)
+        index++
+    }
+
+    if (conditions.length > 0) {
+        query += ` WHERE ` + conditions.join(` AND `)
+    }
+
+    query += ' ORDER BY games.title ASC';
+
+    console.log(query)
+    const { rows } = await pool.query(query, values);
     return rows;
 }
 
