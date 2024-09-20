473a023abe575f351de453d219f369a62f60a729
Karol <kpulawski13@gmail.com>
2024-09-20 20:03:18 +0100

    add publishers to locals

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,8 @@ const db = require('../db/queries');
 async function getMainPage(req, res) {
     const { title, category, publisher } = req.query
     const games = await db.getAllGames(title, publisher, category,);
-    res.render('mainPage', { games: games })
+    const publishers = await db.getAllPublishers();
+    res.render('mainPage', { games: games, publishers })
 }
 
 module.exports = {
