e4c195dc40fd5cdec4d085a812c1ac8a6195da44
Karol <kpulawski13@gmail.com>
2024-09-21 18:13:26 +0100

    add validators to put route

--- routes/game.js
+++ routes/game.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const gameController = require('../controllers/game')
 const gameRouter = express.Router();
-
+const { gameValidations, validate } = require('../validation/updateGame')
 gameRouter.get('/:gameId', gameController.getGameDetails);
 
 gameRouter.post('/', gameController.addGame);
 
-gameRouter.put('/:gameId', gameController.updateGame)
+gameRouter.put('/:gameId', gameValidations(), validate, gameController.updateGame)
 
 gameRouter.delete('/:gameId', gameController.deleteGame);
 
