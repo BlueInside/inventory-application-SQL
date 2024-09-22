bd0f180f7434b3eb89c56077bdcfc6e1aebb3f6d
Karol <kpulawski13@gmail.com>
2024-09-22 20:19:28 +0100

    add async handler

--- routes/game.js
+++ routes/game.js
@@ -2,19 +2,19 @@ const express = require('express');
 const gameController = require('../controllers/game')
 const gameRouter = express.Router();
 const { gameValidations, validate } = require('../validation/updateGame')
-
+const asyncHandler = require('express-async-handler');
 
 
 // gameRouter.post('/', gameController.addGame);
 
-gameRouter.put('/:gameId', gameValidations(), validate, gameController.updateGame)
+gameRouter.put('/:gameId', gameValidations(), validate, asyncHandler(gameController.updateGame))
 
-gameRouter.delete('/:gameId', gameController.deleteGame);
+gameRouter.delete('/:gameId', asyncHandler(gameController.deleteGame));
 
-gameRouter.get('/new', gameController.addGameForm)
+gameRouter.get('/new', asyncHandler(gameController.addGameForm))
 
-gameRouter.get('/:gameId', gameController.getGameDetails);
+gameRouter.get('/:gameId', asyncHandler(gameController.getGameDetails));
 
-gameRouter.get('/:gameId/edit', gameController.editGameDetails)
+gameRouter.get('/:gameId/edit', asyncHandler(gameController.editGameDetails))
 
 module.exports = gameRouter
--- routes/publisher.js
+++ routes/publisher.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
 const publisherRouter = express.Router();
 const publisherController = require('../controllers/publisher')
 const { publisherValidation, validateAndRenderUpdateForm, validateAndRenderPublishers } = require('../validation/addPublisher')
 
-publisherRouter.get('/', publisherController.getAllPublishers);
-publisherRouter.put('/', publisherValidation(), validateAndRenderPublishers, publisherController.changePublisherName)
-publisherRouter.post('/', publisherValidation(), validateAndRenderPublishers, publisherController.addPublisher);
-publisherRouter.delete('/', publisherController.deletePublisher)
+publisherRouter.get('/', asyncHandler(publisherController.getAllPublishers));
+publisherRouter.put('/', publisherValidation(), validateAndRenderPublishers, asyncHandler(publisherController.changePublisherName))
+
+publisherRouter.post('/', publisherValidation(), validateAndRenderPublishers, asyncHandler(publisherController.addPublisher));
+publisherRouter.delete('/', asyncHandler(publisherController.deletePublisher))
 module.exports = publisherRouter
