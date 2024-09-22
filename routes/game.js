7c77f52441f3f9eba3a7435605b50749765ca9ad
Karol <kpulawski13@gmail.com>
2024-09-22 15:40:41 +0100

    change router order

--- routes/game.js
+++ routes/game.js
@@ -2,13 +2,18 @@ const express = require('express');
 const gameController = require('../controllers/game')
 const gameRouter = express.Router();
 const { gameValidations, validate } = require('../validation/updateGame')
-gameRouter.get('/:gameId', gameController.getGameDetails);
 
-gameRouter.post('/', gameController.addGame);
+
+
+// gameRouter.post('/', gameController.addGame);
 
 gameRouter.put('/:gameId', gameValidations(), validate, gameController.updateGame)
 
 gameRouter.delete('/:gameId', gameController.deleteGame);
+
+gameRouter.get('/new', gameController.addGameForm)
+
+gameRouter.get('/:gameId', gameController.getGameDetails);
 
 gameRouter.get('/:gameId/edit', gameController.editGameDetails)
 
