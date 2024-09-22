ab3c06baa1c9eb3a735d944ec2e0251b2cecdff6
Karol <kpulawski13@gmail.com>
2024-09-22 14:23:39 +0100

    add put and delete routes

--- routes/publisher.js
+++ routes/publisher.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const publisherRouter = express.Router();
 const publisherController = require('../controllers/publisher')
-const { publisherValidation, validateAndRenderUpdateForm } = require('../validation/addPublisher')
+const { publisherValidation, validateAndRenderUpdateForm, validateAndRenderPublishers } = require('../validation/addPublisher')
 
 publisherRouter.get('/', publisherController.getAllPublishers);
+publisherRouter.put('/', publisherValidation(), validateAndRenderPublishers, publisherController.changePublisherName)
 publisherRouter.post('/', publisherValidation(), validateAndRenderUpdateForm, publisherController.addPublisher);
-
+publisherRouter.delete('/', publisherController.deletePublisher)
 module.exports = publisherRouter
