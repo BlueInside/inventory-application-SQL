2de6becbe55f406aa04e49471602cc53643a6089
Karol <kpulawski13@gmail.com>
2024-09-22 20:19:45 +0100

    add white space

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,8 @@
 const db = require('../db/queries');
 
+
 async function getMainPage(req, res) {
+
     const { title, category, publisher } = req.query
     const games = await db.getAllGames(title, publisher, category,);
     const publishers = await db.getAllPublishers();
