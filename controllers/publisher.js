b1653cb517edd77372b60882ec1a438a082066a4
Karol <kpulawski13@gmail.com>
2024-09-22 07:53:02 +0100

    create publishers controller

---
+++ controllers/publisher.js
@@ -0,0 +1,20 @@
+const db = require('../db/queries');
+
+async function addPublisher(req, res, next) {
+    const name = req.body.newPublisher;
+    const gameId = req.body.gameId;
+
+    if (!name || !gameId) {
+        throw new Error('Failed to create publisher')
+    }
+
+    // CALL TO DB to create publisher
+    await db.createPublisher(name)
+    // redirect to the edit 
+    res.redirect(`/game/${gameId}/edit`)
+}
+
+module.exports = {
+    addPublisher,
+
+}
